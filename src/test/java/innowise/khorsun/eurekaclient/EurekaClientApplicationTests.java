52e0fa0219bd2d39ae8de1abbdbed704961284cb
mishgan55 <vorota.mogilev@gmail.com>
2023-10-11 14:55:41 +0300

    Initial commit

---
+++ src/test/java/innowise/khorsun/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package innowise.khorsun.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
